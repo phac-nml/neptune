cf73dde40780b852b7128d8f538ca450be7512c5
Eric Marinier <eric.marinier@phac-aspc.gc.ca>
2016-01-19 15:23:38 -0600

    updated Neptune version in setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ dependencies = ['drmaa', 'numpy', 'scipy', 'biopython']
 
 setup(
     name='neptune',
-    version='1.0.0',
+    version='1.1.0',
     url='https://github.com/phac-nml/neptune.git',
     license='Apache-2.0',
     author='Eric Marinier',
