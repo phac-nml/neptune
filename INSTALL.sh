7a73152f4284404944204afb4db7fbd97cf2e128
Eric Marinier <emarinier@gmail.com>
2016-07-06 15:11:08 -0500

    Moving towards a lib and bin install approach.

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,8 +3,32 @@
 # Neptune directory:
 DIR=`dirname $0`
 
+# Install directory:
+PREFIX=$DIR
+
+# Any location provided?
+if [ "$1" != "" ]; then
+    PREFIX=$1
+fi
+
+BIN=$PREFIX/bin
+LIB=$PREFIX/lib
+
+# Create directories if needed:
+if [ ! -d "$PREFIX" ]; then
+    mkdir $PREFIX
+fi
+
+if [ ! -d "$BIN" ]; then
+    mkdir $BIN
+fi
+
+if [ ! -d "$LIB" ]; then
+    mkdir $LIB
+fi
+
 # Create Python virtual environment:
-VENV=$DIR/.venv
+VENV=$LIB/neptune
 virtualenv $VENV
 . $VENV/bin/activate
 
@@ -25,3 +49,7 @@ pip install drmaa
 
 # NEPTUNE
 pip install $DIR/
+
+# BIN
+cp $DIR/neptune.tmp $BIN/neptune
+
--- neptune.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-# Neptune directory:
-DIR=`dirname $0`
-
-. $DIR/.venv/bin/activate
-neptune $@
