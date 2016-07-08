2c395e77002f29300dc9f810e1cb80048deb4198
Eric Marinier <emarinier@gmail.com>
2016-07-08 14:51:50 -0500

    Switching to APT package downloads for all dependencies.

--- install/debian_dependencies.sh
+++ install/debian_dependencies.sh
@@ -1,14 +1,7 @@
 #!/bin/bash 
 
-# PIP
-wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-rm get-pip.py
-
-# VIRTUALENV
-pip install virtualenv
-
-# BUILD ESSENTIAL, PYTHON DEV, BLAST+
+apt-get --yes --force-yes install python-pip
+apt-get --yes --force-yes install python-virtualenv
 apt-get --yes --force-yes install build-essential
 apt-get --yes --force-yes install python-dev
 apt-get --yes --force-yes install ncbi-blast+
