e4249d55e675222a5b65ceb43316ee72e26e5c79
Eric Marinier <emarinier@gmail.com>
2016-09-09 10:54:08 -0500

    Removed setting environment variables.

--- .ci/run_flake8.sh
+++ .ci/run_flake8.sh
@@ -1,9 +1,3 @@
 #!/bin/bash -e
 
-printf "setenv = \n    DRMAA_LIBRARY_PATH = /usr/lib/gridengine-drmaa/lib/libdrmaa.so.1.0\n" >> tox.ini
-printf "    SGE_ROOT = /opt/gridengine\n" >> tox.ini
-printf "    SGE_CELL=default\n" >> tox.ini
-printf "    SGE_QMASTER_PORT=536\n" >> tox.ini
-printf "    SGE_EXECD_PORT=537\n" >> tox.ini
-printf "    SGE_ARCH=linux-x64\n" >> tox.ini
 tox -r -e flake8
